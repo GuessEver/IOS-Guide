442dfa948203627799e75db8dc246e6e7d5a2fa2
Sunnycool <guessever@gmail.com>
2016-04-28 20:21:46 +0800

    GET POST JSON

--- IOS-Guide/http.m
+++ IOS-Guide/http.m
@@ -27,6 +27,7 @@ extern CGRect windowRect;
     [self.urlInputField setBorderStyle:UITextBorderStyleRoundedRect];
     self.urlInputField.autocorrectionType = UITextAutocorrectionTypeNo;
     self.urlInputField.autocapitalizationType = UITextAutocapitalizationTypeNone;
+    self.urlInputField.text = @"http://acm.uestc.edu.cn/problem/search";
     
     UIButton* submitBtnGET = [[UIButton alloc] initWithFrame:CGRectMake(0, 80, windowRect.size.width, 30)];
     [self.view addSubview:submitBtnGET];
@@ -46,6 +47,7 @@ extern CGRect windowRect;
     self.requestInputField.backgroundColor = [UIColor lightTextColor];
     self.requestInputField.autocorrectionType = UITextAutocorrectionTypeNo;
     self.requestInputField.autocapitalizationType = UITextAutocapitalizationTypeNone;
+    self.requestInputField.text = @"{\"currentPage\":\"1\",\"orderAsc\":true,\"orderFields\":\"id\"}";
     
     UIButton* submitBtnPOST = [[UIButton alloc] initWithFrame:CGRectMake(0, 260, windowRect.size.width, 30)];
     [self.view addSubview:submitBtnPOST];
@@ -62,19 +64,58 @@ extern CGRect windowRect;
     [self.view addSubview:self.content];
     self.content.backgroundColor = [UIColor whiteColor];
     self.content.textColor = [UIColor redColor];
+    self.content.numberOfLines = 0;
     
 }
 
 - (void) getReq {
-    NSURL *url = [NSURL URLWithString:self.urlInputField.text];
-    NSURLRequest* request = [NSURLRequest requestWithURL:url];
-    [self.contentPreview loadRequest:request];
-    self.content.text = @"Geting...";
+    NSURL* url = [NSURL URLWithString:self.urlInputField.text];
+    NSMutableURLRequest* request = [NSMutableURLRequest requestWithURL:url];
+    [request setTimeoutInterval:30];
+    [request setHTTPMethod:@"GET"];
+    
+    // GET Request
+    NSURLSession* session = [NSURLSession sharedSession];
+    NSURLSessionDataTask* task = [session dataTaskWithRequest:request completionHandler:^(NSData* data, NSURLResponse* response, NSError* error) {
+        NSLog(@"data:\n%@\n\n", data);
+        NSLog(@"dataString:\n%@\n\n", [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]);
+        NSLog(@"response:\n%@\n\n", response);
+        NSLog(@"error:\n%@\n\n", error);
+        
+        
+        [self.contentPreview loadHTMLString:[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding] baseURL:nil];
+    }];
+    [task resume];
+    
+    self.content.text = @"Getting...";
     [self.content sizeToFit];
 }
 
 - (void) postReq {
+    NSURL* url = [NSURL URLWithString:self.urlInputField.text];
+    NSMutableURLRequest* request = [NSMutableURLRequest requestWithURL:url];
+    [request setTimeoutInterval:10];
+    [request setHTTPMethod:@"POST"];
+    [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    [request setHTTPBody: [self.requestInputField.text dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    //POST Request
+    NSURLSession* session = [NSURLSession sharedSession];
+    NSURLSessionDataTask* task = [session dataTaskWithRequest:request completionHandler:^(NSData* data, NSURLResponse* response, NSError* error) {
+        NSLog(@"data:\n%@\n\n", data);
+        NSLog(@"dataString:\n%@\n\n", [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]);
+        NSLog(@"response:\n%@\n\n", response);
+        NSLog(@"error:\n%@\n\n", error);
+        
+        
+        [self.contentPreview loadHTMLString:[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding] baseURL:nil];
+        //若要引用外部资源，可在baseURL中填写相应网址
+        //[self.contentPreview loadHTMLString:[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding] baseURL:[NSURL URLWithString:@"localhost"]];
+    }];
+    [task resume];
+    
     self.content.text = @"Posting...";
-    [self.content sizeToFit];}
+    [self.content sizeToFit];
+}
 
 @end
